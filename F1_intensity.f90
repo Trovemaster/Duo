9d464a8d8a2ff6e1d0d5ac17d5165dbad821397d
14N16O <qianwei.qu.19@ucl.ac.uk>
2022-03-03 00:27:04 +0800

    Wigner6j(): replace faclog() with log_gamma()

--- F1_intensity.f90
+++ F1_intensity.f90
@@ -18,78 +18,31 @@ contains
                        Nlevels_F1_bra, Nlevels_F1_ket
         REAL(rk), ALLOCATABLE :: parity_conserved_F1_reduced_TDM_matrix(:,:), &
                                  parity_conserved_F1_transitions_matrix(:,:)
-        CHARACTER(LEN=100) :: FMT1 =  "(A22,&
-                                        A22,&
-                                        A22,&
-                                        A8,&
-                                        A22,&
-                                        A6,&
-                                        A6,&
-                                        A6,&
-                                        A7,&
-                                        A6,&
-                                        A6,&
-                                        A4,&
-                                        A7,&
-                                        A6,&
-                                        A6,&
-                                        A8,&
-                                        A6,&
-                                        A6,&
-                                        A7,&
-                                        A6,&
-                                        A6,&
-                                        A4,&
-                                        A7,&
-                                        A6,&
-                                        A6)"
-
-
         CALL F1_hyperfine_structrure
 
+        write(out, '(/A)') "Start: hyperfine transitions calculation"
         open(unit=unit_hyperfine_transitions, file="hyperfine.trans")
 
         ! write(unit_hyperfine_transitions, "(A12, A12, A22, A22, A22)") &
         !     "N_upper", "N_lower", "Einstein-A [s-1]", "nu [cm-1]", "S [Debye^2]"
 
-        ! write(unit_hyperfine_transitions, FMT1) &
-        !     "nu", &
-        !     "Strength", &
-        !     "E", &
-        !     " --> ", &
-        !     "E", &
-        !     " :: ", &
-        !     "F", &
-        !     "I", &
-        !     "parity", &
-        !     "J", &
-        !     "state", &
-        !     "v", &
-        !     "Lambda", &
-        !     "Sigma", &
-        !     "Omega", &
-        !     " --> ", &
-        !     "F", &
-        !     "I", &
-        !     "parity", &
-        !     "J", &
-        !     "state", &
-        !     "v", &
-        !     "Lambda", &
-        !     "Sigma", &
-        !     "Omega" 
-
         do index_F1_bra = 1, num_F1
+            write(out, '(/A4, A, F6.1)') '', 'Start: F_bra =', F1_list(index_F1_bra)
+
             Ndimen_F1_bra = primitive_F1_basis(index_F1_bra)%Ndimen  
 
-            do index_F1_ket = max(1, index_F1_bra - 1), min(num_F1, index_F1_bra + 1) ! F1 selection rule
+            do index_F1_ket = max(1, index_F1_bra - 1), min(num_F1, index_F1_bra + 1) ! F1 selection rules
                 if ((index_F1_bra == index_F1_ket) .and. F1_list(index_F1_bra) == 0) cycle
+
+                write(out, '(/A6, A, F6.1)') '', 'Start: F_ket =', F1_list(index_F1_ket)
                 Ndimen_F1_ket = primitive_F1_basis(index_F1_ket)%Ndimen
 
+                write(out, '(/A8, A)') '',&
+                    'Construct primitive reduced transition dipole moment matrix'
                 ALLOCATE(primitive_F1_reduced_TDM_matrix(Ndimen_F1_bra, Ndimen_F1_ket))
-
                 CALL construct_primitive_F1_reduced_TDM_matrix( &
-                        index_F1_bra, index_F1_ket)   
+                        index_F1_bra, index_F1_ket)
+                write(out, '(A8, A/)') '', '... done'
 
                 do index_represCs_bra = 1, sym%NrepresCs
                     ! parity selection rule
@@ -98,31 +51,40 @@ contains
                     Nlevels_F1_bra = eigen_all_F1(index_F1_bra, index_represCs_bra)%Nlevels
                     Nlevels_F1_ket = eigen_all_F1(index_F1_ket, index_represCs_ket)%Nlevels
 
-                    ALLOCATE(parity_conserved_F1_reduced_TDM_matrix(Nlevels_F1_bra, Nlevels_F1_ket))
-                    
-                    CALL construct_parity_conserved_F1_reduced_TDM_matrix ! TDM: Transition dipole moment
+                    write(out, '(/A10, A)') '',&
+                        'Construct parity conserved reduced transition dipole moment matrix'
+                    ALLOCATE(parity_conserved_F1_reduced_TDM_matrix(Nlevels_F1_bra, Nlevels_F1_ket))                    
+                    CALL construct_parity_conserved_F1_reduced_TDM_matrix
+                    write(out, '(A10, A/)') '', '... done'
 
-                    ALLOCATE(parity_conserved_F1_transitions_matrix(Nlevels_F1_bra, Nlevels_F1_ket))
-                    ! wavenumber_to_MHz = wavenumber_to_MHz
+                    write(out, '(/A10, A)') '',&
+                        'Calculate and print transitions'
+                    ALLOCATE(parity_conserved_F1_transitions_matrix(Nlevels_F1_bra, Nlevels_F1_ket))                    
                     CALL construct_parity_conserved_F1_transitions_matrix
+                    write(out, '(A10, A/)') '', '... done'
                     
                     DEALLOCATE(parity_conserved_F1_transitions_matrix)
                     DEALLOCATE(parity_conserved_F1_reduced_TDM_matrix)
                 end do           
                 
                 DEALLOCATE(primitive_F1_reduced_TDM_matrix)
-            end do            
+
+                write(out, '(/A6, A, F6.1)') '', 'End: F_ket =', F1_list(index_F1_ket)
+            end do 
+            
+            write(out, '(/A4, A, F6.1)') '', 'End: F_bra =', F1_list(index_F1_bra)
         end do
 
         close(unit=unit_hyperfine_transitions)
-
+        write(out, '(/A)') "End: hyperfine transitions calculation"
     contains
         subroutine construct_parity_conserved_F1_reduced_TDM_matrix
         ! | <psi_m^{tau,F}| T^1(mu) |psi_n^{tau',F'}> | ** 2
         implicit none
 
         REAL(rk), ALLOCATABLE :: intermediate_matrix(:, :)
-            ! ! ^{tau,F}M^{tau', F'} = Psi^{tau, F} * ^{F}M^{F'} * Psi^{tau', F'} 
+            ! See Eq.(58) of DOI: 10.1021/acs.jctc.1c01244
+            ! ! ^{tau,F}M^{tau', F'} = {Psi^{tau, F}}^\dagger * ^{F}M^{F'} * Psi^{tau', F'} 
             ! parity_conserved_F1_reduced_TDM_matrix = &
             !     matmul(matmul(transpose(eigen_all_F1(index_F1_bra, index_represCs_bra)%vect), &
             !                 primitive_F1_reduced_TDM_matrix), &
@@ -130,7 +92,7 @@ contains
 
             ALLOCATE(intermediate_matrix(Nlevels_F1_bra, Ndimen_F1_ket))
 
-            ! M_indermediate} = Psi^{tau, F} * ^{F}M^{F'}
+            ! M_indermediate = {Psi^{tau, F}}^\dagger * ^{F}M^{F'}
             CALL dgemm('T', 'N', Nlevels_F1_bra, Ndimen_F1_ket, Ndimen_F1_bra, 1.0_rk, &
                         eigen_all_F1(index_F1_bra, index_represCs_bra)%vect, Ndimen_F1_bra, &
                         primitive_F1_reduced_TDM_matrix, Ndimen_F1_bra, &
@@ -144,48 +106,18 @@ contains
 
             DEALLOCATE(intermediate_matrix)
 
+            ! See Eq.(57) of DOI: 10.1021/acs.jctc.1c01244
             parity_conserved_F1_reduced_TDM_matrix = &
                 parity_conserved_F1_reduced_TDM_matrix ** 2
         end subroutine construct_parity_conserved_F1_reduced_TDM_matrix
         
         subroutine construct_parity_conserved_F1_transitions_matrix
             implicit none
-            INTEGER(ik) :: bra, ket, maxloc_ket, maxloc_bra
-            TYPE(quantaT), POINTER :: quanta_bra, quanta_ket   
+            INTEGER(ik) :: bra, ket  
             REAL(rk)  :: EinsteinA
             
-            CHARACTER(LEN=120) :: FMT2 =  "(F22.12,&
-                                            E22.12,&
-                                            F22.12,&
-                                            A8,&
-                                            F22.12,&
-                                            A6,&
-                                            F6.1,&
-                                            F6.1,&
-                                            A7,&
-                                            F6.1,&
-                                            I6,& 
-                                            I4,&
-                                            I7,&
-                                            F6.1,&
-                                            F6.1,&
-                                            A8,&
-                                            F6.1,&
-                                            F6.1,&
-                                            A7,&
-                                            F6.1,&
-                                            I6,& 
-                                            I4,&
-                                            I7,&
-                                            F6.1,&
-                                            F6.1)"
-
-            do ket = 1, Nlevels_F1_ket                
-                ! maxloc_ket = MAXLOC(ABS(eigen_all_F1(index_F1_ket, index_represCs_ket)%vect(:, ket)), DIM=1) 
-                ! quanta_ket => primitive_F1_basis(index_F1_ket)%icontr(maxloc_ket)
+            do ket = 1, Nlevels_F1_ket
                 do bra = 1, Nlevels_F1_bra
-                    ! maxloc_bra = MAXLOC(ABS(eigen_all_F1(index_F1_bra, index_represCs_bra)%vect(:, bra)), DIM=1) 
-                    ! quanta_bra => primitive_F1_basis(index_F1_bra)%icontr(maxloc_bra)
 
                     parity_conserved_F1_transitions_matrix(bra, ket) = &
                         eigen_all_F1(index_F1_bra, index_represCs_bra)%val(bra) &
@@ -209,33 +141,6 @@ contains
                         EinsteinA, &
                         parity_conserved_F1_transitions_matrix(bra, ket), &
                         parity_conserved_F1_reduced_TDM_matrix(bra, ket)
-
-                    ! WRITE(unit_hyperfine_transitions, FMT2) &
-                    !     parity_conserved_F1_transitions_matrix(bra, ket), &
-                    !     parity_conserved_F1_reduced_TDM_matrix(bra, ket), &
-                    !     eigen_all_F1(index_F1_bra, index_represCs_bra)%val(bra), &
-                    !     " --> ", &
-                    !     eigen_all_F1(index_F1_ket, index_represCs_ket)%val(ket), &
-                    !     " :: ", &
-                    !     quanta_bra%F1, &
-                    !     quanta_bra%I1, &
-                    !     trim(parity_sign(index_represCs_bra)), &
-                    !     quanta_bra%Jrot, &
-                    !     quanta_bra%istate, &
-                    !     quanta_bra%v, &
-                    !     quanta_bra%ilambda, &
-                    !     quanta_bra%sigma, &
-                    !     quanta_bra%omega, &
-                    !     " --> ", &
-                    !     quanta_ket%F1, &
-                    !     quanta_ket%I1, &
-                    !     trim(parity_sign(index_represCs_ket)), &
-                    !     quanta_ket%Jrot, &
-                    !     quanta_ket%istate, &
-                    !     quanta_ket%v, &
-                    !     quanta_ket%ilambda, &
-                    !     quanta_ket%sigma, &
-                    !     quanta_ket%omega
                 end do    
             end do 
         end subroutine construct_parity_conserved_F1_transitions_matrix
@@ -311,31 +216,10 @@ contains
                                 S_bra, S_ket, Sigma_bra, Sigma_ket, &
                                 J_bra, J_ket, Omega_bra, Omega_ket
         INTEGER(ik), INTENT(IN) :: index_field, index_v_bra, index_v_ket
-        INTEGER(ik) :: t, q
+        INTEGER(ik) :: q
         REAL(rk) :: tdm
 
-        ! Kato
-        ! if ( (nint(S_ket - S_bra) == 0 ) &
-        !     .and.  (nint(Sigma_bra - Sigma_ket) == 0) ) then
-        !     do t = -1, 1
-        !         if ( nint(Omega_bra - Omega_ket - t) == 0 ) then
-        !             tdm = get_sign(-F1_bra+I1-Omega_ket) &
-        !                 * sqrt((2.0_rk * F1_bra + 1.0_rk) &
-        !                         * (2.0_rk * F1_ket + 1.0_rk) &
-        !                         * (2.0_rk * J_bra + 1.0_rk) &
-        !                         * (2.0_rk * J_ket + 1.0_rk) &
-        !                         ) &
-        !                 * Wigner6j(F1_bra, F1_ket, 1.0_rk, J_ket, J_bra, I1) &
-        !                 * get_sign(-real(t,rk)) &
-        !                 * mu_v() &
-        !                 * Wigner3j(J_bra, J_ket, 1.0_rk, &
-        !                             Omega_bra, -Omega_ket, -real(t,rk))
-        !             exit
-        !         end if
-        !     end do
-        ! else
-        !     tdm = 0
-        ! end if
+        ! See Eqs. (59) to (61) of DOI: 10.1021/acs.jctc.1c01244
 
         tdm = 0
         if ( (nint(S_ket - S_bra) == 0 ) &
